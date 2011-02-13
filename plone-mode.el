f1e329b2b34ab3382928887269d7889732d02f38
Jonas Baumann <jone@jone.ch>
2011-02-13 18:23:32 +0100

    implemented plone-run-zope-foreground feature

--- plone-mode.el
+++ plone-mode.el
@@ -31,4 +31,90 @@
 
 (require 'json)
 
+;;;; custom
+
+(defgroup plone-mode nil
+  "plone-mode customization"
+  :group 'applications)
+
+(defcustom plone-zope-instance-paths "bin/instance
+bin/instance0
+bin/instanceadm
+bin/instance1"
+  "Zope instance paths relative to buildout root."
+  :type 'string
+  :group 'plone-mode)
+
+
+
+;;;; funs
+
+(defun plone--find-parent-with-file (path filename)
+  "Traverse PATH upwards until we find FILENAME in the dir.
+
+If we find it return the path of that dir, othwise nil is
+returned."
+  (if (file-exists-p (concat path "/" filename))
+      path
+    (let ((parent-dir (file-name-directory (directory-file-name path))))
+      ;; Make sure we do not go into infinite recursion
+      (if (string-equal path parent-dir)
+          nil
+        (plone--find-parent-with-file parent-dir filename)))))
+
+
+(defun plone--find-buildout-root (path)
+  "Search PATH for a buildout root.
+
+If a buildout root is found return the path, othwise return
+nil."
+  ;; find the most top one, not the first one
+  (let* ((dir default-directory)
+         (previous dir))
+    (while (not (equalp dir nil))
+      (setq dir (find-parent-with-file dir "bootstrap.py"))
+      (if (not (equalp dir nil))
+          (progn
+            (setq previous dir)
+            ;; get parent dir
+            (setq dir (file-name-directory (directory-file-name dir))))))
+    (message (concat "Found buildout at: " previous))
+    previous))
+
+
+(defun plone-run-zope-foreground ()
+  "Run zope serer in foreground."
+  (interactive)
+
+  (let ((buildout-directory (plone--find-buildout-root default-directory))
+        (instances (split-string plone-zope-instance-paths "\n")))
+
+    (while instances
+      (let ((path (concat buildout-directory (car instances))))
+        (when (file-exists-p path)
+          (pdb (concat path " fg"))
+          (setq instances nil)))
+      (setq instances (cdr instances)))))
+
+;;;; keymap
+
+(defvar plone-mode-map ()
+  "Keymap for plone-mode.")
+
+(if plone-mode-map ()
+  (setq plone-mode-map (make-sparse-keymap))
+  (define-prefix-command 'plone-mode 'plone-map)
+  (define-key plone-mode-map (kbd "M-p") plone-map)
+
+  (define-key plone-mode-map (kbd "M-p f") 'plone-run-zope-foreground))
+
+;;;; mode
+
+(define-minor-mode plone-mode
+  "Toggle mrsd mode."
+  :global t
+  :keymap plone-mode-map
+  :lighter " Plone")
+
+
 (provide 'plone-mode)
